5cef6f917da08766adce35401987debe63c845e3
phb2000 <p.becker2000@hotmail.com>
2023-05-08 20:47:04 -0300

    feat: ensure returns conflict exception when email already exists

--- src/main/java/com/project/transferapi/application/CreateUser.java
+++ src/main/java/com/project/transferapi/application/CreateUser.java
@@ -2,6 +2,7 @@ package com.project.transferapi.application;
 
 import com.project.transferapi.domain.entity.User;
 import com.project.transferapi.domain.exceptions.ConflictException;
+import com.project.transferapi.domain.ports.IFindUserByEmail;
 import com.project.transferapi.domain.ports.IFindUserByLegalDocumentNumber;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class CreateUser {
 
+    private final IFindUserByEmail findUserByEmail;
     private final IFindUserByLegalDocumentNumber findUserByLegalDocumentNumber;
 
     public Long invoke(final User user) {
-        this.findUserByLegalDocumentNumber.find(user.getLegalDocumentNumber()).ifPresent(s -> {
-            throw new ConflictException("given cnpj already registered");
+        this.findUserByLegalDocumentNumber.find(user.getLegalDocumentNumber()).ifPresent(u -> {
+            throw new ConflictException("given legal document number already registered");
+        });
+
+        this.findUserByEmail.find(user.getEmail()).ifPresent(u -> {
+            throw new ConflictException("given email already registered");
         });
 
         return null;
