0a99b1bfb1a4fe6e03694b95fe52f3b1115d18fa
phb2000 <p.becker2000@hotmail.com>
2023-05-09 23:42:39 -0300

    feat: add dto erros classes

---
+++ src/main/java/com/project/transferapi/interfaces/inbound/http/handler/ExceptionDetails.java
@@ -0,0 +1,20 @@
+package com.project.transferapi.interfaces.inbound.http.handler;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@SuperBuilder
+public class ExceptionDetails {
+
+    protected String title;
+    protected int status;
+    protected String details;
+    protected LocalDateTime timestamp;
+    protected String developerMessage;
+
+}
