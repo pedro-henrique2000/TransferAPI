dc76ada587f800887bb69d2f9c883221316ddc47
phb2000 <p.becker2000@hotmail.com>
2023-05-10 13:53:41 -0300

    feat: adding logs

--- src/main/java/com/project/transferapi/interfaces/outbound/http/ExternalTransactionAuthorizerService.java
+++ src/main/java/com/project/transferapi/interfaces/outbound/http/ExternalTransactionAuthorizerService.java
@@ -3,11 +3,13 @@ package com.project.transferapi.interfaces.outbound.http;
 import com.project.transferapi.domain.ports.IExternalTransactionAuthorizer;
 import com.project.transferapi.interfaces.outbound.dto.ExternalAuthorizerResponseDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import static java.util.Objects.nonNull;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class ExternalTransactionAuthorizerService implements IExternalTransactionAuthorizer {
@@ -17,7 +19,10 @@ public class ExternalTransactionAuthorizerService implements IExternalTransactio
 
     @Override
     public boolean invoke() {
+        log.info("ExternalTransactionAuthorizerService::invoke - Sending request to external authorizer service");
         ResponseEntity<ExternalAuthorizerResponseDTO> response = this.externalTransactionAuthorizerFeignClient.invoke();
+        log.info("ExternalTransactionAuthorizerService::invoke - Sent request to external authorizer service. Received Response {}", response.getStatusCode());
+
         ExternalAuthorizerResponseDTO responseBody = response.getBody();
 
         return response.getStatusCode().value() == 200
