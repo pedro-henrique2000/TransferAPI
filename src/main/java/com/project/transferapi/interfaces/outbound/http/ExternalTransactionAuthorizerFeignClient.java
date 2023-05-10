d5868bc7afb8f0d8af8667a73c4edfeb2d9c2b71
phb2000 <p.becker2000@hotmail.com>
2023-05-10 17:54:00 -0300

    feat: adding feign client

--- src/main/java/com/project/transferapi/interfaces/outbound/http/ExternalTransactionAuthorizerFeignClient.java
+++ src/main/java/com/project/transferapi/interfaces/outbound/http/ExternalTransactionAuthorizerFeignClient.java
@@ -1,8 +1,12 @@
 package com.project.transferapi.interfaces.outbound.http;
 
 import com.project.transferapi.interfaces.outbound.dto.ExternalAuthorizerResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@FeignClient(url = "", name = "transaction-authorizer")
 public interface ExternalTransactionAuthorizerFeignClient {
+    @GetMapping
     ResponseEntity<ExternalAuthorizerResponseDTO> invoke();
 }
