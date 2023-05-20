a242c896955876834be4833a3890d1808d4c76c0
phb2000 <p.becker2000@hotmail.com>
2023-05-20 12:01:40 -0300

    refactor: refactor

--- src/main/java/com/project/transferapi/interfaces/outbound/http/ExternalTransactionAuthorizerFeignClient.java
+++ src/main/java/com/project/transferapi/interfaces/outbound/http/ExternalTransactionAuthorizerFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(url = "https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6", name = "transaction-authorizer")
+@FeignClient(url = "", name = "transaction-authorizer")
 public interface ExternalTransactionAuthorizerFeignClient {
     @GetMapping
     ResponseEntity<ExternalAuthorizerResponseDTO> invoke();
