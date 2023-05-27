3e6df4d14054add1fdb88f307c02cddb09a31fcc
phb2000 <p.becker2000@hotmail.com>
2023-05-27 16:04:37 -0300

    feat: adding open api to login controller

--- src/main/java/com/project/transferapi/interfaces/inbound/http/controller/LoginController.java
+++ src/main/java/com/project/transferapi/interfaces/inbound/http/controller/LoginController.java
@@ -3,6 +3,11 @@ package com.project.transferapi.interfaces.inbound.http.controller;
 import com.project.transferapi.application.AuthenticateUser;
 import com.project.transferapi.interfaces.inbound.http.dto.AuthenticationRequest;
 import com.project.transferapi.interfaces.inbound.http.dto.AuthenticationResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "User Management", description = "User Management Endpoints")
 public class LoginController {
 
     private final AuthenticateUser authenticateUser;
 
     @PostMapping()
+    @Operation(
+            description = "Authenticate Endpoint",
+            summary = "Authenticate Endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Success Response",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = AuthenticationResponse.class))
+                    )
+            })
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody @Valid AuthenticationRequest authenticationRequest) {
         String token = this.authenticateUser.invoke(
                 authenticationRequest.getEmail(),
