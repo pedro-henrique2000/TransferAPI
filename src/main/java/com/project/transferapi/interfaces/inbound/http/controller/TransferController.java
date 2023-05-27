19a58b0d8306f016144c2527d055c6a51070bced
phb2000 <p.becker2000@hotmail.com>
2023-05-27 14:39:21 -0300

    feat: validate source id and authenticated user id on transfer

--- src/main/java/com/project/transferapi/interfaces/inbound/http/controller/TransferController.java
+++ src/main/java/com/project/transferapi/interfaces/inbound/http/controller/TransferController.java
@@ -5,6 +5,7 @@ import com.project.transferapi.interfaces.inbound.http.dto.TransferAmountDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class TransferController {
 
     private final TransferAmount transferAmount;
 
+    @PreAuthorize("#transferAmountDTO.sourceId == authentication.principal.id")
     @PostMapping
     public ResponseEntity<Void> transfer(@RequestBody @Valid TransferAmountDTO transferAmountDTO) {
         this.transferAmount.invoke(
