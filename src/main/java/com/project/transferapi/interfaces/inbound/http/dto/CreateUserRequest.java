a337315a9e58101de60dedfd051835f4bcbd0c8e
phb2000 <p.becker2000@hotmail.com>
2023-05-27 17:51:32 -0300

    feat: validate fields size

--- src/main/java/com/project/transferapi/interfaces/inbound/http/dto/CreateUserRequest.java
+++ src/main/java/com/project/transferapi/interfaces/inbound/http/dto/CreateUserRequest.java
@@ -3,7 +3,9 @@ package com.project.transferapi.interfaces.inbound.http.dto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,23 +15,28 @@ import static java.util.Objects.isNull;
 @Setter
 public class CreateUserRequest {
 
+    @Size(max = 50)
     @NotBlank
     @Schema(description = "User Full Name", example = "John Doe")
     private String fullName;
 
     @NotBlank
+    @Size(max = 14)
     @Schema(description = "User Legal Document Number", example = "123456789")
     private String legalDocumentNumber;
 
     @NotBlank
+    @Size(max = 50)
     @Schema(description = "User Email", example = "mail@mail.com")
     private String email;
 
     @NotBlank
+    @Size(max = 50)
     @Schema(description = "User Password", example = "123456")
     private String password;
 
     @NotBlank
+    @Size(max = 50)
     @Schema(description = "Password Confirmation (same as password)", example = "123456")
     private String passwordConfirmation;
 
